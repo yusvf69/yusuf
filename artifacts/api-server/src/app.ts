35d5d2f31b98626f518ab8153d376ac5407ac7ef
youssef mohamed <youssefmohamed@youssefs-MacBook-Pro.local>
2026-04-02 16:41:50 +0200

    first commddddwswit

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp, { pinoHttp as pinoHttpLogger } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,14 +10,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: Request) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
